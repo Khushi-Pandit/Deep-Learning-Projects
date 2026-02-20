c62d44c96163d09906568fbb519ecc566282cdc4
Khushi Pandit <khushipandit0201@gmail.com>
2026-02-20 21:06:30 +0530

    feat: thresholding done

--- retinal-vessel-extraction/src/thresholding.py
+++ retinal-vessel-extraction/src/thresholding.py
@@ -1,1 +1,12 @@
-e
+import numpy as np
+from skimage.filters import threshold_niblack, threshold_sauvola
+
+def apply_niblack(image, window_size=25, k=0.2):
+    thresh = threshold_niblack(image, window_size=window_size, k=k)
+    binary = image > thresh
+    return binary.astype(np.uint8) * 255
+
+def apply_sauvola(image, window_size=25, k=0.2):
+    thresh = threshold_sauvola(image, window_size=window_size, k=k)
+    binary = image > thresh
+    return binary.astype(np.uint8) * 255
